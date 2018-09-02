0d7e40ae215a5140cfbbcaed3306310cae315363
Denis Chernishev <denis.chernishev.7@gmail.com>
2018-09-03 00:36:11 +0300

    Админ часть merge. merge files local

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\UploadController as Upload;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,19 +25,9 @@ class HomeController extends Controller
 	 */
 	public function index()
 	{
-		$filesArray = Upload::getFiles('public/images');
-		$files = array();
-		foreach ($filesArray as $value){
-			if(strpos($value, 'public/images/image')===0){
-				$value = str_replace('public/', '',$value);
-				$files[] = $value;
-			}
-		}
-		
 		$data = [
 			  'title' => 'Главная страница',
-				'files' => $files,
 		];
-		return view('index', $data);
+		return view('welcome', $data);
 	}
 }
