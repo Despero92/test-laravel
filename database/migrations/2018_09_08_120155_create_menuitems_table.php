2422103adfaa4ec08802fa8163bcc1824fddd3d4
Denis Chernishev <denis.chernishev.7@gmail.com>
2018-09-29 01:56:50 +0300

    Get Slider Images Сделал получение и вывод картинок в слайдере

---
+++ database/migrations/2018_09_08_120155_create_menuitems_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuitemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menuitems', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('label', 100);
+            $table->text('params');
+            $table->tinyInteger('enable' )->default('0');
+            $table->smallInteger('order' );
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menuitems');
+    }
+}
